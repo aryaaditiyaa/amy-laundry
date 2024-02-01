0d1b0856ee4a15531888de2554039baea4cf8d54
Arya Aditiya <aryaaditiya15@gmail.com>
2024-02-01 21:26:16 +0700

    feat: add contact us section

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -4,7 +4,7 @@
 @else
     <footer class="p-4 bg-white md:p-8 lg:p-10 dark:bg-gray-800">
         <div class="mx-auto max-w-screen-xl text-center">
-            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="{{ route('home') }}" class="hover:underline">Home Laundry™</a>. All Rights Reserved.</span>
+            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {{ date('Y') }} <a href="{{ route('home') }}" class="hover:underline">Home Laundry™</a>. All Rights Reserved.</span>
         </div>
     </footer>
 @endif
